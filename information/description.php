9b2e8aacf168f0bf3bd817ebb8733561950608a4
Sam Hazeldine <sdh42@bath.ac.uk>
2017-03-22 19:29:10 +0000

    Photos
    Added photos to "description.php"
    Included photos

--- information/description.php
+++ information/description.php
@@ -10,13 +10,18 @@
 	}
 
     //Create SQL query
-    $entry = "EDGE"; //Change for a variable that is passed into the code.
-    $query = "SELECT name, fullinfo, links FROM utbpoi WHERE name = '$entry' ";
+    $entry = "SU"; //Change for a variable that is passed into the code.
+    $query = "SELECT name, fullinfo, links, photo FROM utbpoi WHERE name = '$entry' ";
     $result = $connection->query($query);
     $result->fetchInto($col);
     $name = $col[0];
     $description = $col[1];
     $links = $col[2];
+    $photo = $col[3];
+    $prePhoto = "photos/";
+    $photo = $prePhoto.$photo;
+
+
 ?>
 
 <head>
@@ -25,16 +30,14 @@
 
 <body>
     <h1>
-        <?php
-            echo $name;
-        ?>
+        <?php echo $name; ?>
     </h1>
     <p>
-        <?php
-            echo $description;
-        ?>
+        <?php echo $description; ?>
     </p>
     <p>
-        <a href = <?php echo $links; ?> > More information </a> 
+        <a href = <?php echo $links; ?> > More information </a>
     </p>
+    <img src = <?php echo $photo; ?> />
+
 </body>
